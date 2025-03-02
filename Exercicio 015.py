78b737d0ebf1bf20af82cad211728fe5108bfda6
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-03-02 13:22:48 -0300

    Update Exercicio 015.py

--- Exercicio 015.py
+++ Exercicio 015.py
@@ -1,1 +1,9 @@
+n1 = int(input("Digite os KM rodados: "))
+n2 = int(input("Digite a quantidade de dias que você alugou o carro?: "))
+
+n3 = n1 * 0.15
+n4 = n2 * 60
+n5 = n3 + n4
+
+print("Você tera que pagar um total de R${:.2f}".format(n5))
 
