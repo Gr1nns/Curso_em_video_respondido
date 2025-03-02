0b84f85e9018584cfb041ff6b849a9ea90b32ef0
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-03-02 13:24:05 -0300

    Update Exercicio 019.py

--- Exercicio 019.py
+++ Exercicio 019.py
@@ -1,1 +1,10 @@
+import math
+n1 = float(input("Digite o angulo: "))
 
+se = math.sin(math.radians(n1))
+co = math.cos(math.radians(n1))
+ta = math.tan(math.radians(n1))
+
+print("O seno é {:.2f}".format(se))
+print("O cosseno é {:.2f}".format(co))
+print("E a tangente é {:.2f}".format(ta))
