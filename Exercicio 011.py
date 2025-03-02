b7fc650444db05c614de55b262be0dfed84ed48d
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-03-02 13:21:41 -0300

    Update Exercicio 011.py

--- Exercicio 011.py
+++ Exercicio 011.py
@@ -1,1 +1,7 @@
-
+n1 = float(input("Digite a altura da parede: "))
+n2 = float(input("Digite a largura da parede: "))
+a = n1 * n2
+p = n1 + n1 + n2 + n2
+lt = a / 2
+print("A area da parede é igual a {:2}, com um perimetro igual a {}".format(a, p))
+print("A quantidade de tinta em litros que vc ira gastar para pintar a parede é {:0} Litros".format(lt))
