ad691c1a6560a4b74c64dd10d319c5d1d1229deb
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-03-02 13:20:32 -0300

    Update Exercicio 008.py

--- Exercicio 008.py
+++ Exercicio 008.py
@@ -1,1 +1,5 @@
-
+n1 = int(input("Digite um valor em metro: "))
+n2 = n1 * 100
+n3 = n1 * 1000
+print("O valor de {}m em centimetros é igual a {}cm.".format(n1, n2))
+print("O valor de {}m em milimetros, é igual a {}mm.".format(n1, n3))
