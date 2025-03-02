49e8dfa455795c2b13305893e41c180f64234d20
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-03-02 13:21:57 -0300

    Update Exercicio 012.py

--- Exercicio 012.py
+++ Exercicio 012.py
@@ -1,1 +1,3 @@
-
+n1 = float(input("Digite o valor do produto: R$"))
+n2 = (n1 * 0.05) - n1
+print("Nós estamos com um desconto de 5% em todos os produtos, agora você irá pagar apenas R${}".format(abs(n2)))
