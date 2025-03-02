6dffb1be510d197136451b44823fdc73b8db5fef
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-03-02 13:20:02 -0300

    Update Exercicio 006.py

--- Exercicio 006.py
+++ Exercicio 006.py
@@ -1,1 +1,8 @@
-
+import math
+n1 = int(input("Digite um número: "))
+n2 = n1 * 2
+n3 = n1 * 3
+n4 = math.sqrt(n1)
+print("O dobro de {} é igual a {}.".format(n1, n2))
+print("O triplo de {} é igual a {}.".format(n1, n3))
+print("A raiz quadrade de {}, é igual a {}.".format(n1, n4))
