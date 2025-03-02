2597331891bed9fd538a130be5579777c32c7ed4
Reyner Dittert <152820684+Gr1nns@users.noreply.github.com>
2025-03-02 13:23:26 -0300

    Update Exercicio 017.py

--- Exercicio 017.py
+++ Exercicio 017.py
@@ -1,1 +1,4 @@
-
+n1 = float(input("Digite o valor do cateto oposto: "))
+n2 = float(input("Digite o valor do cateto adjacente: "))
+n3 = (n1 ** 2 + n2 ** 2) ** (1/2)
+print("A hipotenusa é {:.2f}".format(n3))
